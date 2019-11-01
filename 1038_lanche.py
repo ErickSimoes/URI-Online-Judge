7c9b4a21a0b2bebdc3210fc56a533b7f5d5339e0
Erick Simões <erick.simoes@live.com>
2019-11-01 11:29:58 -0300

    Resolução do desafio 1038

---
+++ 1038_lanche.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+pedido, quant = input().split(" ")
+pedido, quant = int(pedido), float(quant)
+valor = 0
+
+if pedido == 1:
+    valor = 4.0
+elif pedido == 2:
+    valor = 4.5
+elif pedido == 3:
+    valor = 5.0
+elif pedido == 4:
+    valor = 2.0
+elif pedido == 5:
+    valor = 1.5
+
+print('Total: R$ {:.2f}'.format(valor * quant))
+
